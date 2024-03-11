978c36bce68ba73abababdcdcf6f8edcb9223ee2
Whitney Simpson <whitmoon4819@gmail.com>
2024-03-11 12:04:57 -0400

    starting to book search engine, still need to figure out why frontend will not add new user to backend

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,41 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id })
+      }
+      throw AuthenticationError;
+    },
+    users: async () =>{
+      return User.find({})
+    }
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  }
+};
+
+module.exports = resolvers;
